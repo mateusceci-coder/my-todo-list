62d7f05b79ec112e42fd99a62239e842ecc5e12d
Mateus Ceci <mateusceci@gmail.com>
2023-11-04 16:31:45 -0300

    build: add lucide react

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ export default function App() {
       <header className="flex justify-center items-center py-6">
         <h1 className="text-main-text-dark text-2xl font-agbalumo">My Todo App</h1>
       </header>
+      <div>
+        
+      </div>
     </div>
   )
 }
