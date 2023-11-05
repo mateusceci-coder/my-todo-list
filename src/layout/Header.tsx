d788127a41ef35deeac76f5a283d2ec5a674acd8
Mateus Ceci <mateusceci@gmail.com>
2023-11-05 11:16:50 -0300

    style: add new Header layout

---
+++ src/layout/Header.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Header() {
+    return (
+        <header className="flex justify-center items-center py-6">
+            <h1 className="text-main-text-dark text-5xl font-agbalumo">My Todo App</h1>
+        </header>
+    )
+}
