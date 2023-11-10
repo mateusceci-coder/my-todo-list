f9d3521a0dde06292304efde3fdae18314bcbbf0
Mateus Ceci <mateusceci@gmail.com>
2023-11-10 20:34:53 -0300

    fix: color bug

--- src/components/StatusItem.tsx
+++ src/components/StatusItem.tsx
@@ -3,10 +3,19 @@ type StatusItemProps = {
 }
 
 
+
 export default function StatusItem({status}: StatusItemProps) {
+
+    const changeBg = () => {
+        if(status === "urgent") return '#DC2626'
+        if (status === "important") return 'rgb(30, 64, 175)'
+        if (status === "normal") return 'rgb(22, 163, 74)'
+        else return "rgb(202, 138, 4)"
+    }
+
     return (
         <div className="flex justify-end">
-            <p className={`p-1 bg-${status} rounded-2xl text-main-text`}>{status.charAt(0).toUpperCase()+status.slice(1)}</p>
+            <p className="p-1 rounded-2xl  text-main-text" style={{backgroundColor : changeBg()}}>{status.charAt(0).toUpperCase()+status.slice(1)}</p>
         </div>
     )
 }
