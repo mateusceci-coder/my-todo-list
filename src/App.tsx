b98cc161d7216675d7ce0da1f0c1ce803904ee51
Mateus Ceci <mateusceci@gmail.com>
2023-11-15 21:10:08 -0300

    chore: add material ui and switch button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import {  useFormTodoContext } from './contexts/Formcontext'
 import Input from './components/Input'
 import TodoList from './layout/TodoList'
 import Formbar from './layout/Formbar'
+import Switch from "@mui/material/Switch"
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function App() {
   const {  isFormOpen, isInputOpen, handleDarkMode, darkMode } = useFormTodoContext()
@@ -10,10 +13,14 @@ export default function App() {
   return (
   <div className={`${darkMode && "dark"}`}>
     <div className="bg-white dark:bg-main h-screen w-100 text-main-text p-3 overflow-y-scroll relative">
-      <button onClick={handleDarkMode}>Dark Mode</button>     
+      <div className='flex gap-2 fixed left-10 top-5'>
+      <FormGroup>
+        <FormControlLabel className='dark:text-white text-black' control={<Switch onChange={handleDarkMode} defaultChecked />} label="Dark Mode" />
+      </FormGroup>
+      </div>
       {isFormOpen && <div className="bg-main bg-opacity-70 absolute inset-0"></div>}
       <Header />
-      {isInputOpen ? 
+        {isInputOpen ? 
           <Input /> : <Formbar />
         }
       <TodoList />
